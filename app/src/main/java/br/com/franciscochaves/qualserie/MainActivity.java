1872d7d8fce1ea53a22d52204716ed29b928b6d0
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-20 16:14:40 -0300

    Realizando a troca de imagem com SeekBar

--- app/src/main/java/br/com/franciscochaves/qualserie/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/qualserie/MainActivity.java
@@ -2,12 +2,52 @@ package br.com.franciscochaves.qualserie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekBar;
+    private ImageView mImageExibicao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar_serie);
+        mImageExibicao = findViewById(R.id.image_serie);
+
+       mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+           @Override
+           public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+               int valorProgesso = i;
+
+               switch (valorProgesso){
+                   case 1:
+                       mImageExibicao.setImageResource(R.drawable.pouco);
+                       break;
+                   case 2:
+                       mImageExibicao.setImageResource(R.drawable.medio);
+                       break;
+                   case 3:
+                       mImageExibicao.setImageResource(R.drawable.muito);
+                       break;
+                   case 4:
+                       mImageExibicao.setImageResource(R.drawable.susto);
+                       break;
+               }
+           }
+
+           @Override
+           public void onStartTrackingTouch(SeekBar seekBar) {
+
+           }
+
+           @Override
+           public void onStopTrackingTouch(SeekBar seekBar) {
+
+           }
+       });
     }
 }
